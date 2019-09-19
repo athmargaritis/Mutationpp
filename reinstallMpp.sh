4c511b81de8334e1ba4a7f79ce84bf0cd291b21d
Thanos Margaritis <a.margaritis@imperial.ac.uk>
2019-09-19 16:58:08 +0100

    added prefix for install

--- reinstallMpp.sh
+++ reinstallMpp.sh
@@ -5,7 +5,7 @@ mkdir build install
 python scripts/install_dependencies.py
 
 cd build
-cmake .. -DBUILD_FORTRAN_WRAPPER=ON
+cmake .. -DBUILD_FORTRAN_WRAPPER=ON -DCMAKE_INSTALL_PREFIX="~/repositories/mutationpp/install"
 
 make -j install
 cd ..
